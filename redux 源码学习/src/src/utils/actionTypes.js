532752b6a2c96c0e8889be2881b74540f91e28da
dongliang1993 <dongliangfe@gmail.com>
2019-02-27 17:30:44 +0800

    更新 actionTypes 注释

--- redux 源码学习/src/src/utils/actionTypes.js
+++ redux 源码学习/src/src/utils/actionTypes.js
@@ -3,6 +3,9 @@
  * For any unknown actions, you must return the current state.
  * If the current state is undefined, you must return the initial state.
  * Do not reference these action types directly in your code.
+ * 这些都是 Redux 自己内部的 action types
+ * 所以在自己的代码里面千万不要用这些内部的 type
+ * 对于没有匹配到的 action types，要走默认的 default case
  */
 
 const randomString = () =>
